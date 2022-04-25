b54c7b20661cc4c59d3024a050d8db053a93e3dd
Colin Tang <53540424+cot2005@users.noreply.github.com>
2022-04-25 11:49:46 -0700

    Update EasyBoxplots.R

--- EasyBoxplots.R
+++ EasyBoxplots.R
@@ -1,40 +1,48 @@
 # author: Colin Tang
 # simple function to make box plots from csv's (default) where
-# each parameter number defines the column for the value.
+# each parameter number defines the column for the variable to be plotted. NULL shape or fill variables will
+# adjust the output graph. 
 #
 
 library("ggplot2")
 
-ggEasy.boxplot4<-function(inputFile, xval = 1, yval = 2, shape = 3, fill = 4, sep = ",", 
+ggEasy.boxplot<-function(inputFile, xval = 1, yval = 2, shape = NULL, fill = NULL, sep = ",", angle.X = TRUE, 
+                          box.width = 1, point.size = 2.5,
                           outputName = "boxPlot.pdf", height = 5, width = 6, facet = FALSE) {
-  df <- read.csv(inputFile, stringsAsFactors = T, sep = sep)
+  df <- read.csv(inputFile, stringsAsFactors = T, sep = sep, check.names = F)
   header <- colnames(df)
   for (i in c(xval, shape, fill)) {
     df[,i] <- as.factor(df[,i])
   }
-  g <- ggplot(df, aes(x = df[,xval], y = df[,yval], fill = df[,fill]))
-  if (facet == TRUE) {   # adds facet wrapper
+  if (is.null(fill) == TRUE && is.null(shape) == TRUE) {   # 2 var
+    g <- ggplot(df, aes(x = df[,xval], y = df[,yval]))
+    g <- g + geom_boxplot(alpha = 0.5, color = "black", width = box.width) + 
+      geom_point(aes(group = df[,xval]), shape = 21, alpha = 0.8, size = point.size) + 
+      theme_bw() + labs(x = header[xval], y = header[yval])
+  } else if (is.null(fill) == TRUE) {   # 3 var null fill/ no grouping
+    g <- ggplot(df, aes(x = df[,xval], y = df[,yval]))
+    g <- g + geom_boxplot(alpha = 0.5, color = "black", width = box.width) + 
+      geom_point(aes(shape = df[,shape]), alpha = 0.8, size = point.size, position=position_jitterdodge(dodge.width = box.width, jitter.width = box.width/2)) + 
+      scale_shape_manual(values = 21:(20 + length(levels(df[,shape])))) + theme_bw() + labs(x = header[xval], y = header[yval]) + 
+      guides(shape = guide_legend(title=header[shape]))
+  } else if (is.null(shape) == TRUE) {   # 3 var null shape
+    g <- ggplot(df, aes(x = df[,xval], y = df[,yval], fill = df[,fill]))
+    g <- g + geom_boxplot(alpha = 0.5, color = "black", width = box.width) + 
+      geom_point(aes(group = df[,fill]), shape = 21, alpha = 0.8, size = point.size, position=position_jitterdodge(dodge.width = box.width, jitter.width = box.width/2)) + 
+      theme_bw() + labs(x = header[xval], y = header[yval]) + guides(fill = guide_legend(title=header[fill]))
+  } else {   # all 4 var
+    g <- ggplot(df, aes(x = df[,xval], y = df[,yval], fill = df[,fill]))
+    g <- g + geom_boxplot(alpha = 0.5, color = "black", width = box.width) + 
+      geom_point(aes(group = df[,fill], shape = df[,shape]), alpha = 0.8, size = point.size, position=position_jitterdodge(dodge.width = box.width, jitter.width = box.width/2)) + 
+      scale_shape_manual(values = 21:(20 + length(levels(df[,shape])))) + theme_bw() + labs(x = header[xval], y = header[yval]) + 
+      guides(shape = guide_legend(title=header[shape]), fill = guide_legend(title=header[fill]))
+  }
+  if (angle.X == TRUE) {   # angles X axis labels
+    g <- g + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+  }
+  if (facet == TRUE && is.null(fill) == FALSE) {   # adds facet wrapper
     g <- g + facet_grid(cols = vars(df[,fill]))
   }
-  g + geom_boxplot(alpha = 0.5, color = "black") + geom_point(aes(group = df[,fill], shape = df[,shape]), alpha = 0.8, size = 2.5, position=position_jitterdodge()) + 
-    scale_shape_manual(values = 21:(20 + length(levels(df[,shape])))) + theme_bw() + labs(x = header[xval], y = header[yval]) + 
-    guides(shape = guide_legend(title=header[shape]), fill = guide_legend(title=header[fill]))
-  ggsave(outputName, height = height, width = width)
-}
-
-ggEasy.boxplot3<-function(inputFile, xval = 1, yval = 2, fill = 3, sep = ",", 
-                          outputName = "boxPlot.pdf", height = 5, width = 6, facet = FALSE) {
-  df <- read.csv(inputFile, stringsAsFactors = T, sep = sep)
-  header <- colnames(df)
-  for (i in c(xval, fill)) {
-    df[,i] <- as.factor(df[,i])
-  }
-  g <- ggplot(df, aes(x = df[,xval], y = df[,yval], fill = df[,fill]))
-  if (facet == TRUE) {   # adds facet wrapper
-    g <- g + facet_grid(cols = vars(df[,fill]))
-  }
-  g + geom_boxplot(alpha = 0.5, color = "black") + 
-    geom_point(aes(group = df[,fill]), shape = 21, alpha = 0.8, size = 2.5, position=position_jitterdodge()) + 
-    theme_bw() + labs(x = header[xval], y = header[yval]) + guides(fill = guide_legend(title=header[fill]))
+  print(g)
   ggsave(outputName, height = height, width = width)
 }
