d5b66f9d9fac6f1dab69583b677d0e4899970857
Colin Tang <53540424+cot2005@users.noreply.github.com>
2022-05-02 14:06:26 -0700

    Update EasyCorrelationScatter.R

--- EasyCorrelationScatter.R
+++ EasyCorrelationScatter.R
@@ -1,13 +1,12 @@
-ggEasyCorPlot<-function(df, xval = 1, yval = 2, fill = 3, label = 4, facetGroup = NULL, cortest = "pearson", point.size = 2.5, width = 8, height = 8, outputfile = "facet_corplot.pdf") {
+ggEasy.CorPlot<-function(df, xval = 1, yval = 2, fill = 3, label = 4, facetGroup = NULL, cortest = "pearson", point.size = 2.5, width = 8, height = 8, outputfile = "facet_corplot.pdf") {
   cols <- colnames(df)
   #puts correlation result in upper left quadrant
-  xlabcoord <- (max(df[,xval]) - min(df[,xval])) * 0.1 + min(df[,xval])
+  xlabcoord <- (max(df[,xval]) - min(df[,xval])) * 0.1 + min(df[,xval]).  # used to place the corr annotation
   ylabcoord <- (max(df[,yval]) - min(df[,yval])) * 0.95 + min(df[,yval])
 
   g <- ggplot(df, aes(x = df[,xval], y = df[,yval], label = df[,label]))
   g <- g + geom_point(aes(fill = df[,fill]), alpha = 0.8, size = point.size, shape = 21) + theme(legend.position = "none") + 
     theme_bw() + guides(fill = guide_legend(title=cols[fill])) + geom_text_repel(size = 3, box.padding = 0.5, max.overlaps = 100) + labs(x = cols[xval], y = cols[yval]) 
-    #guides(fill = guide_legend(title=cols[fill])) #+ scale_y_continuous(limits = c(0,1.50),breaks = seq(0, 1.50, by = 0.25)) 
   # calculates correlations
   corrList <- c()
   if (is.null(facetGroup) == FALSE) {   # adds facet wrapper
