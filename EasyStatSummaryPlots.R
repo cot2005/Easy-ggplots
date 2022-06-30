a8d883fe4cf6a4c51b1aca3ce1bd73c11aa0e43b
Colin Tang <53540424+cot2005@users.noreply.github.com>
2022-06-29 20:27:20 -0700

    Update EasyStatSummaryPlots.R

--- EasyStatSummaryPlots.R
+++ EasyStatSummaryPlots.R
@@ -11,15 +11,15 @@ library(wesanderson)
 ggEasy.scatter.stat<-function(df, xval = 1, yval = 2, color = 3, errortype = "sd", connectLine = FALSE, 
                               error.width = 0.15, line.size = 0.5, point.size = 1,alpha = 0.8, width = 8, height = 6,
                               wespalette = "Darjeeling1") {
-  g <- ggplot(df, aes(x = df[,xval], y = df[,yval], color = df[,color]))
+  g <- ggplot(df, aes(x = df[,xval], y = df[,yval], color = factor(df[,color]), group = factor(df[,color])))
   if (errortype == "cl") {
   g <- g + stat_summary(fun.data = mean_cl_boot, alpha = alpha, size = point.size)
   } else {
     g <- g + stat_summary(fun = mean, geom = "point", alpha = alpha, size = point.size)
     if (errortype == "sd") {
-      g <- g + stat_summary(fun.data = mean_sdl, geom = "errorbar", width = error.width, size = error.width * 3, alpha = alpha)
+      g <- g + stat_summary(fun.data = mean_sdl, geom = "errorbar", width = error.width, size = error.width * 4, alpha = alpha)
     } else if (errortype == "sem") {
-      g <- g + stat_summary(fun.data = mean_se, geom = "errorbar", width = error.width, size = error.width * 3, alpha = alpha)
+      g <- g + stat_summary(fun.data = mean_se, geom = "errorbar", width = error.width, size = error.width * 4, alpha = alpha)
     }
   }
   if (connectLine == TRUE) {   # adds mean line
@@ -36,10 +36,10 @@ ggEasy.scatter.stat<-function(df, xval = 1, yval = 2, color = 3, errortype = "sd
 
 # sd = standard deviation
 # sem = standard error of the mean
-ggEasy.barplot.stat<-function(df, xval = 1, yval = 2, color = 3, shape = NULL, errortype = "sd", allpoints = T,
+ggEasy.barplot.stat<-function(df, xval = 1, yval = 2, color = 3, shape = NULL, group = color, errortype = "sd", allpoints = T,
                               error.width = 0.15, point.size = 1, alpha = 0.8, width = 8, height = 6,
                               wespalette = "Darjeeling1") {
-  g <- ggplot(df,aes(x = factor(df[,xval]), y = df[,yval], color = factor(df[,color]), fill = factor(df[,color]))) + 
+  g <- ggplot(df,aes(x = factor(df[,xval]), y = df[,yval], color = factor(df[,color]), fill = factor(df[,color]), group = factor(df[,group]))) + 
     stat_summary(fun="mean",position="dodge",geom="bar", width = error.width, alpha = alpha)
   if (errortype == "sd") {
     g <- g + stat_summary(fun.data=mean_sdl, position=position_dodge(error.width), geom="errorbar", width = error.width/2, alpha = alpha, color = "black")
@@ -56,7 +56,6 @@ ggEasy.barplot.stat<-function(df, xval = 1, yval = 2, color = 3, shape = NULL, e
     }
   g <- g + guides(fill = guide_legend(title = colnames(df)[color]), color = "none") + 
     labs(x = colnames(df)[xval], y = colnames(df)[yval]) + theme_bw() +
-    scale_color_manual(values = wes_palette(wespalette, length(unique(df[,color], type = "continuous")))) + 
     scale_fill_manual(values = wes_palette(wespalette, length(unique(df[,color], type = "continuous"))))
   print(g)
   ggsave("statBarPlot.pdf", plot = g, width = width, height = height)
