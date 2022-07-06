381ba506bb2256a6883610d157d5947aa09dfb2e
Colin Tang <53540424+cot2005@users.noreply.github.com>
2022-07-06 15:01:21 -0700

    Update EasyHistogram.R

--- EasyHistogram.R
+++ EasyHistogram.R
@@ -5,20 +5,24 @@
 
 library("ggplot2")
 
-ggEasy.histogram<-function(inputFile, xval = 1, yaxis = "count", sep = ",", binNum = 30, graphColor = "#46ACC8", 
+ggEasy.histogram<-function(df, xval = 1, yaxis = "count", fill = NA, sep = ",", alpha = 0.5, binNum = 30, graphColor = "#46ACC8", 
                            outputName = "HistogramPlot.pdf", print = FALSE, print.height = 5, print.width = 7) {
   # flexible inputs
   headers <- colnames(df)
   xval <- ifelse(is.numeric(xval), xval, which(headers == xval))
-  
-  g <- ggplot(inputData, aes(inputData[,xval]))
-  if (yaxis == "count") {
-    g <- g + geom_histogram(aes(y=..count..), alpha=0.7, color = "black", fill = graphColor, size = 0.25, binwidth = max(inputData[,xval])/binNum)
+  fill <- ifelse(is.numeric(fill), fill, which(headers == fill))
+  if (is.na(fill)) {
+    g <- ggplot(df, aes(df[,xval]))
   } else {
-    g <- g + geom_density(alpha=0.4, color = "black", fill = graphColor, size = 0.25) +
-      geom_histogram(aes(y=..density..), color = "black", fill = graphColor, alpha=0.7, size = 0.25, binwidth = max(inputData[,xval])/binNum)
+    g <- ggplot(df, aes(x = df[,xval], fill = df[,fill]))
   }
-  g <- g + theme_bw() + xlab(colnames(inputData)[xval]) + scale_x_continuous(breaks = scales::pretty_breaks(n = 8))
+  if (yaxis == "count") {
+    g <- g + geom_histogram(aes(y=..count..), alpha=alpha, color = "black", fill = graphColor, size = 0.25, binwidth = max(df[,xval])/binNum)
+  } else {
+    g <- g + geom_density(alpha=alpha, color = "black", fill = graphColor, size = 0.25) +
+      geom_histogram(aes(y=..density..), color = "black", fill = graphColor, alpha=alpha, size = 0.25, binwidth = max(df[,xval])/binNum)
+  }
+  g <- g + theme_bw() + xlab(colnames(df)[xval]) + scale_x_continuous(breaks = scales::pretty_breaks(n = 8))
   
   if (print == TRUE) {
     ggsave(outputName, plot = g, width = print.width, height = print.height)
