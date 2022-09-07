01ceec88a18f12042bdbcc4c421aa91fbf35617b
Colin Tang <53540424+cot2005@users.noreply.github.com>
2022-09-07 15:29:24 -0700

    Create EasyUMAP.R

---
+++ EasyUMAP.R
@@ -0,0 +1,37 @@
+library(umap)
+library(ggplot2)
+library(wesanderson)
+
+ggEasy.umap<-function(df, labelColumn = ncol(df), point.size = 2, alpha = 0.8,
+                      palette = wes_palette("Darjeeling1", numLabels, type = "continuous"),
+                      print = FALSE, outputName = "umap.pdf", print.width = 7, print.height = 6) {
+  # flexible inputs
+  headers <- colnames(df)
+  labelColumn <- ifelse(is.numeric(labelColumn), labelColumn, which(headers == labelColumn))
+  
+  # separates labels and gets stats
+  labels <- df[,labelColumn]
+  df <- df[,-labelColumn]
+  numLabels <- length(unique(labels))
+  
+  # performs clustering
+  umapData <- umap(df)
+  
+  # reattaches labels
+  umap.df <- data.frame(UMAP1 = umapData$layout[,1], UMAP2 = umapData$layout[,2], 
+                        Label = labels)
+  combinedValues <- c(umap.df[,1], umap.df[,2])
+  umap.max <- max(combinedValues)
+  umap.max <- umap.max * ifelse(umap.max > 0, 1.1, 0.9)
+  umap.min <- min(combinedValues) * 0.9
+  umap.min <- umap.min * ifelse(umap.min < 0, 1.1, 0.9)
+  #performs plotting
+  g <- ggEasy.scatter4(umap.df, xval = 1, yval = 2, color = 3, shape = 3, point.size = point.size, 
+                       alpha = alpha, palette = palette) + coord_cartesian(xlim = c(umap.min,umap.max), ylim = c(umap.min,umap.max)) + 
+    theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+  if (print == TRUE) {
+    ggsave(outputName, plot = g, width = print.width, height = print.height)
+  }
+  return(g)
+}
