8e3873ee96c28ac5dd7065e1598d9203e5d65fb5
Colin Tang <53540424+cot2005@users.noreply.github.com>
2022-09-11 21:37:01 -0700

    Update t-SNE.R

--- t-SNE.R
+++ t-SNE.R
@@ -26,7 +26,7 @@ ggEasy.tsne<-function(df, labelColumn = ncol(df), max_iter = 500, exaggeration_f
   tsne <- Rtsne(df, dims = 2, perplexity=perplexity, verbose=F, max_iter=max_iter, exaggeration_factor=exaggeration_factor,
                 eta=learning, check_duplicates = F)
   tsne.df <- data.frame(dim1 = tsne$Y[,1], dim2 = tsne$Y[,2], Label = as.factor(labels))
-  g <- ggEasy.scatter4(tsne.df, xval = 1, yval = 2, color = 3, shape = 3, point.size = point.size, 
+  g <- ggEasy.scatter(tsne.df, xval = 1, yval = 2, color = 3, shape = 3, point.size = point.size, 
                        alpha = alpha, palette = palette) + 
     theme(panel.border = element_blank(), panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
