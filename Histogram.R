bdd4a26baf5e3357c2be15323cd605b820538f70
Colin Tang <53540424+cot2005@users.noreply.github.com>
2023-04-15 12:31:27 -0700

    Update Histogram.R

--- Histogram.R
+++ Histogram.R
@@ -4,27 +4,51 @@
 #
 
 library("ggplot2")
+library("wesanderson")
 
-ggEasy.histogram<-function(df, xval = 1, yaxis = "count", fill = NA, sep = ",", alpha = 0.5, binNum = 30, bars = FALSE,
+ggEasy.histogram<-function(df, xval = 1, yaxis = "count", color = NA, sep = ",", legend = TRUE,
+                           alpha = 0.5, binNum = 30, density.bars = FALSE, line.size = 0.25, 
+                           palette = wes_palette(palette.wes_palette, length(unique(df[,color])), type = "continuous"),
+                           palette.wes_palette = "Darjeeling1",
                            outputName = "histogramPlot.pdf", print = FALSE, print.height = 5, print.width = 7) {
+  df <- as.data.frame(df)
   # flexible inputs
   headers <- colnames(df)
   xval <- ifelse(is.numeric(xval), xval, which(headers == xval))
-  fill <- ifelse(is.numeric(fill), fill, which(headers == fill))
-  if (is.na(fill)) {
-    g <- ggplot(df, aes(df[,xval]))
-  } else {
-    g <- ggplot(df, aes(x = df[,xval], fill = df[,fill])) + guides(fill = guide_legend(title=headers[fill]))
+  color <- ifelse(is.numeric(color), color, which(headers == color))
+  
+  # adds filler column for NA purposes
+  if (is.na(color)) {
+    df$FillerColor <- "filler"
+    color <- ncol(df)
   }
-  if (yaxis == "count") {
-    g <- g + geom_histogram(aes(y=..count..),  position="identity", alpha=alpha, color = "black", size = 0.25, binwidth = max(df[,xval])/binNum)
-  } else {
-    g <- g + geom_density(alpha=alpha, color = "black", size = 0.25)
-    if (bars == TRUE) {
-      g <- g + geom_histogram(aes(y=..density..), position="identity", color = "black", alpha=alpha, size = 0.25, binwidth = max(df[,xval])/binNum)
+    
+  # constructs ggplot object
+  g <- ggplot(df)
+  
+  g <- ggplot(df, aes(x = df[,xval], fill = factor(df[,color]), group = factor(df[,color])))
+  if (yaxis == "count") {   # count histogram
+    g <- g + geom_histogram(aes(y=..count..),  position="identity", 
+                            alpha = alpha, size = line.size, color = "black",
+                            binwidth = max(df[,xval], na.rm = T)/binNum)
+  } else {   # assumes density if not count
+    g <- g + geom_density(alpha=alpha, color = "black",
+                          size = line.size)
+    if (density.bars == TRUE) {
+      g <- g + geom_histogram(aes(y=..density..), position="identity", 
+                              color = "black", alpha=alpha, size = line.size, 
+                              binwidth = max(df[,xval], na.rm = T)/binNum)
     }
   }
-  g <- g + theme_bw() + xlab(colnames(df)[xval]) + scale_x_continuous(breaks = scales::pretty_breaks(n = 8))
+  g <- g + theme_bw() + scale_x_continuous(breaks = scales::pretty_breaks(n = 8)) +
+    xlab(headers[xval]) + 
+    scale_fill_manual(values = palette)
+  
+  if ("FillerColor" %in% colnames(df)) {
+    g <- g + theme(legend.position = "none")
+  } else {
+    g <- g + guides(fill = guide_legend(title = headers[color]))
+  }
   
   if (print == TRUE) {
     ggsave(outputName, plot = g, width = print.width, height = print.height)
